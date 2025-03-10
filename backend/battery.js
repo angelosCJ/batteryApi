83b89b8215b84ca7b2ec275b086957d7aefc9430
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2025-03-10 23:06:12 +0300

    Update battery.js

--- backend/battery.js
+++ backend/battery.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const batteryDataSchema = new mongoose.Schema({
     deviceName:{type:String,required:true},
-    batteryLevel:{type:String,required:true},
+    batteryLevel:{type:Number,required:true},
 });
 
 module.exports  = mongoose.model("Battery",batteryDataSchema);
